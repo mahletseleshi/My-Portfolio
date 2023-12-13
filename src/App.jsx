fa684b13076cafdf1f31606a26939daef8e863db
Mahlet Seleshi <mahletseleshi224@gmail.com>
2023-12-13 21:16:47 +0300

    updated page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Gallery from "./components/Gallery"
 import Socials from "./components/Socials"
 import About from "./components/About"
 import Portfolio from "./components/Portfolio"
+import Experience from "./components/Experience"
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
      <Socials />
      <About />
      <Portfolio />
+     <Experience />
     </div>
 
   )
---
+++ src/components/Experience.jsx
@@ -0,0 +1,94 @@
+import Adobe from "../assets/Adobe illustrator.png";
+import Indesign from "../assets/Adobe InDesign.png";
+import Canva from "../assets/Canva.png";
+import CSS3 from "../assets/CSS3.png";
+import GitHub from "../assets/GitHub.png";
+import HTML from "../assets/HTML.png";
+import JavaScript from "../assets/JavaScript.png";
+import Next from "../assets/Next JS.png";
+import Photoshop from "../assets/Photoshop.png";
+import React from "../assets/React.png";
+import Tailwind from "../assets/Tailwind CSS.png";
+import Wordpress from "../assets/Wordpress.png";
+import { IoBagHandleSharp } from "react-icons/io5";
+
+const Experience = () => {
+  return (
+    <div className="mt-44 flex flex-col  relative justify-center w-full my-24 px-32">
+        <div className="flex flex-col items-center justify-between">
+      <h1 className="text-[32px] text-[#0F2A3D]">EXPERIENCE</h1>
+      <h1 className="text-[70px] absolute -top-6 text-black/20">EXPERIENCE</h1>
+      </div>
+      <div className="flex flex-col items-start justify-start"> 
+        <div className="flex items-center justify-center">
+          <div className="flex items-center flex-col justify-start">
+            <h2 className="text-3xl">Front End Developer </h2>
+        
+          <div className="flex">
+            <p>Kuraz Technologies</p>
+            <p>2020-present</p>
+          </div>
+          </div>
+          <div>
+            <IoBagHandleSharp />
+          </div>
+          <div>
+            <h2>2023</h2>
+          </div>
+        </div>
+
+
+
+        <div>
+          <div>
+            <h2>Front End Developer </h2>
+            <div>
+              <p>Kuraz Technologies</p>
+              <p>2020-present</p>
+            </div>
+            <div>
+              <IoBagHandleSharp />
+            </div>
+            <div>
+              <h2>2023</h2>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <h2>Front End Developer </h2>
+            <div>
+              <p>Kuraz Technologies</p>
+              <p>2020-present</p>
+            </div>
+            <div>
+              <IoBagHandleSharp />
+            </div>
+            <div>
+              <h2>2023</h2>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <h2>Front End Developer </h2>
+            <div>
+              <p>Kuraz Technologies</p>
+              <p>2020-present</p>
+            </div>
+            <div>
+              <IoBagHandleSharp />
+            </div>
+            <div>
+              <h2>2023</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
