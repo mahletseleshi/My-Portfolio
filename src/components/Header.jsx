c2b71d69cc2d706fd3e741e58431aa77c4b677b9
Mahlet Seleshi <mahletseleshi224@gmail.com>
2023-12-18 21:06:33 +0300

    updated page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,8 +17,8 @@ const Header = () => {
     <div id="Header" className="overflow-hidden bg-gradient-to-r box-border  w-full h-screen from-[#0F2A3D] flex justify-between items-center  px-4 md:px-32 to-[#3F5564] text-white">
       <div className="flex flex-col space-y-5 items-center justify-center md:justify-start md:items-start">
         <p className="text-[25px] md:text-[15px]">Hello, I am Mahlet👋 </p>
-        <h1 className="text-5xl w-[70%] text-center md:text-left md:text-[65px] md:w-[80%] md:leading-[1.1] font-semibold">
-          I am a <span className="text-Yellow ">{text}</span> <Cursor cursorStyle="<"/>
+        <h1 className="text-5xl w-[70%] text-center md:text-left md:text-[65px] md:w-[90%] md:leading-[1.1] font-semibold">
+          I am a <span className="text-Yellow ">{text}</span> <span><Cursor cursorStyle="/"/></span>
         </h1>
         <p className="w-[70%] text-[20px] md:text-[14px] text-center md:text-left">
           An experienced frontend developer with enthusiastic perception of
