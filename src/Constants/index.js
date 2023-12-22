dc43699ff2eb84dfe219b21d73555604b9abb3f9
Mahlet Seleshi <mahletseleshi224@gmail.com>
2023-12-22 15:29:31 +0300

    updated page

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -3,42 +3,45 @@
 import Kuraz from "../assets/Kuraz.png"
 import Planton from "../assets/Planton.png"
 import Netflix from "../assets/Netflix.png"
+import Emani from "../assets/Emani.png"
 
 export const items = [
     {
       id: 1,
       backgroundImg: Kuraz,
       title: "Kuraz Exam",
-      Description: `A website made for movie enthusiasts. It came into life using Tailwind CSS and React JS.`,
-      Demo: `https://www.w3schools.com/cssref/css3_pr_transform-origin.php`,
-      code: `https://tailwindcss.com/docs/rotate`,
+      Description: `A website students to excel in their entrance examinations. It came into life using React JS.`,
+      Demo: `https://kuraz-exam-lgrx.vercel.app/`,
+      code: `https://github.com/mahletseleshi/Kuraz-exam`,
     },
 
     {
       id: 2,
       backgroundImg: Planton,
       title: `Planton App`,
-      Description: `A website made for movie enthusiasts. It came into life using Tailwind CSS and React JS.`,
-      Demo: `https://www.w3schools.com/cssref/css3_pr_transform-origin.php`,
-      code: `https://tailwindcss.com/docs/rotate`,
+      Description: `A website made for plant lovers. It came into life using Tailwind CSS and React JS.`,
+      Demo: `https://planton.vercel.app/`,
+      code: `https://github.com/mahletseleshi/Planton`,
     },
 
     {
       id: 3,
       backgroundImg: Netflix,
       title: `Netflix-Clone`,
-      Description: `A website made for movie enthusiasts. It came into life using Tailwind CSS and React JS.`,
-      Demo: `https://www.w3schools.com/cssref/css3_pr_transform-origin.php`,
-      code: `https://tailwindcss.com/docs/rotate`,
+      Description: `
+      A Netflix clone with Tailwind CSS and React allowing users to browse content with a responsive interface. `,
+      Demo: `https://netflix-clone-k83u2bz7l-mahletseleshi.vercel.app/`,
+      code: `https://github.com/mahletseleshi/netflix-clone`,
     },
 
     {
       id: 4,
-      backgroundImg: Kuraz,
-      title: `Kuraz Exam`,
-      Description: `A website made for movie enthusiasts. It came into life using Tailwind CSS and React JS.`,
-      Demo: `https://www.w3schools.com/cssref/css3_pr_transform-origin.php`,
-      code: `https://tailwindcss.com/docs/rotate`,
+      backgroundImg: Emani ,
+      title: `Emani App`,
+      Description: `
+      A restaurant app, crafted with modern design using React brings the essence of a dining experience to users' fingertips`,
+      Demo: `https://food-app-csms.vercel.app/`,
+      code: `https://github.com/mahletseleshi/Food-App`,
     },
 
     {
@@ -49,4 +52,6 @@ export const items = [
       Demo: `https://www.w3schools.com/cssref/css3_pr_transform-origin.php`,
       code: `https://tailwindcss.com/docs/rotate`,
     },
+
+    
   ];
